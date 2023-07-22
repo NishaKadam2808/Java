73e991ecbfe3beaa9a2b298cbb0b3409246267dd
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-22 19:20:55 +0530

    Implicit_Explicit

---
+++ Type Casting/pr4a.java
@@ -0,0 +1,15 @@
+import java .io.*;
+import java .util.*;
+class pr4a
+{
+	public static void main(String arg[])
+	{
+		int i=10;
+		
+		long b=i;
+		
+		System.out.println(b);
+		
+	}
+
+}
---
+++ Type Casting/pr4b.java
@@ -0,0 +1,15 @@
+import java .io.*;
+import java .util.*;
+class pr4b
+{
+	public static void main(String arg[])
+	{
+		int i=10;
+		
+		float b=(float)i;
+		
+		System.out.println(b);
+		
+	}
+
+}
