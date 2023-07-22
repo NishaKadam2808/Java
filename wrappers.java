8b3c67e63f4e394fe31b0b45a0bc47ec60e0d1e3
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-22 19:41:02 +0530

    wrapper_class

---
+++ wrappers.java
@@ -0,0 +1,14 @@
+import java .io.*;
+import java .util.*;
+
+
+class wrappers
+{
+	public static void main(String arg[])
+	{
+		String i="100";
+		int a=Integer.parseInt(i);
+		System.out.println(a);
+	}
+
+}
