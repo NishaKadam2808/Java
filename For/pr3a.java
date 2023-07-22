c0e734663f8baa309683b5a0c352a7b832ef61ae
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-22 19:19:04 +0530

    For_loop

---
+++ For/pr3a.java
@@ -0,0 +1,17 @@
+import java .io.*;
+import java .util.*;
+class pr3a
+{
+	public static void main(String arg[])
+	{
+		int i;
+		for(i=0;i<=10;i++)
+		{
+		if(i%2==0)
+		{
+		System.out.println(i);
+		}
+		} 
+	}
+
+}
---
+++ For/pr3b.java
@@ -0,0 +1,16 @@
+import java .io.*;
+import java .util.*;
+class pr3b
+{
+	public static void main(String arg[])
+	{
+		int i=1;
+		do
+		{
+		
+		System.out.println(i);
+		i++;
+		} while(i<=50);
+	}
+
+}
