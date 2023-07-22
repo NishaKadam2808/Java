9e1cb5ffad717a38623c6a8b5e0c14caaf0a4978
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-22 19:24:13 +0530

    Command_Line_Argument

---
+++ pr7.java
@@ -0,0 +1,14 @@
+import java .io.*;
+import java .util.*;
+
+
+class pr7
+{
+	public static void main(String arg[])
+	{
+		String i="100";
+		int a=Integer.parseInt(i);
+		System.out.println(a);
+	}
+
+}
